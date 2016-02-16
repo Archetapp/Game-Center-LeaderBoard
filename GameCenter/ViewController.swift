38198f5c5ac68c1d57d09b072ea86e31042a1a8c
Archetapp <archetapp@gmail.com>
2016-02-16 13:03:50 -0700

    LeaderBoard

---
+++ GameCenter/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  GameCenter
+//
+//  Created by Jared Davidson on 2/15/16.
+//  Copyright © 2016 Archetapp. All rights reserved.
+//
+
+import UIKit
+import GameKit
+
+class ViewController: UIViewController, GKGameCenterControllerDelegate {
+
+    @IBOutlet var Label: UILabel!
+    
+    var score = Int()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        authPlayer()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func SddScore(sender: AnyObject) {
+        score++
+        Label.text = "\(score)"
+    }
+
+    @IBAction func CallGC(sender: AnyObject) {
+        saveHighscore(score)
+        showLeaderBoard()
+    }
+    
+    
+    func authPlayer(){
+        let localPlayer = GKLocalPlayer.localPlayer()
+        
+        localPlayer.authenticateHandler = {
+            (view, error) in
+            
+            if view != nil {
+                
+                self.presentViewController(view!, animated: true, completion: nil)
+                
+            }
+            else {
+                
+                print(GKLocalPlayer.localPlayer().authenticated)
+                
+            }
+            
+            
+        }
+    }
+    
+    
+    func saveHighscore(number : Int){
+        
+        if GKLocalPlayer.localPlayer().authenticated {
+            
+            let scoreReporter = GKScore(leaderboardIdentifier: "This")
+            
+            scoreReporter.value = Int64(number)
+            
+            let scoreArray : [GKScore] = [scoreReporter]
+            
+            GKScore.reportScores(scoreArray, withCompletionHandler: nil)
+            
+        }
+        
+        
+    }
+    
+    func showLeaderBoard(){
+        let viewController = self.view.window?.rootViewController
+        let gcvc = GKGameCenterViewController()
+        
+        gcvc.gameCenterDelegate = self
+        
+        viewController?.presentViewController(gcvc, animated: true, completion: nil)
+    }
+    
+    
+    func gameCenterViewControllerDidFinish(gameCenterViewController: GKGameCenterViewController) {
+        gameCenterViewController.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
